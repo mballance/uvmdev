8a06bda269a75371d66cebb52d347ee4cb1af4c3
mballance-sf <matt.ballance@gmail.com>
2018-09-24 10:00:54 -0700

    X

--- src/uvmdev_mgr.svh
+++ src/uvmdev_mgr.svh
@@ -9,14 +9,18 @@ typedef class uvmdev_if;
  * 
  * TODO: Add class documentation
  */
-class uvmdev_mgr;
-	static uvmdev_mgr		m_inst = new();
+class uvmdev_mgr extends uvm_component;
+	`uvm_component_utils(uvmdev_mgr)
+	
+	static uvmdev_mgr		m_inst = null;
 	uvmdev_if				m_devices[int unsigned];
 	uvm_object				m_dev_data[int unsigned];
 	uvmdev_mem_if			m_mem_if;
+	uvm_phase				m_run_phase;
 
-	function new();
-
+	function new(string name, uvm_component parent);
+		super.new(name, parent);
+		m_inst = this;
 	endfunction
 
 	function void add_device(
@@ -36,14 +40,28 @@ class uvmdev_mgr;
 		end
 	endfunction
 	
+	function void raise_objection(uvm_object obj, string description="", int count=1);
+		if (m_run_phase != null) begin
+			m_run_phase.raise_objection(obj, description, count);
+		end else begin
+			`uvm_fatal("uvmdev_mgr", "Attempting to raise an objection on a null handle");
+		end
+	endfunction
+	
+	function void drop_objection(uvm_object obj, string description="", int count=1);
+		if (m_run_phase != null) begin
+			m_run_phase.drop_objection(obj, description, count);
+		end else begin
+			`uvm_fatal("uvmdev_mgr", "Attempting to drop an objection on a null handle");
+		end
+	endfunction
+	
 	task init_devices();
 		int unsigned idx;
 		
 		if (m_devices.first(idx)) begin
 			do begin
-				$display("--> Init %p", m_devices[idx]);
 				m_devices[idx].init(this, idx);
-				$display("<-- Init %p", m_devices[idx]);
 			end while (m_devices.next(idx));
 		end
 	endtask
@@ -57,8 +75,15 @@ class uvmdev_mgr;
 	endfunction
 	
 	static function uvmdev_mgr inst();
+		if (m_inst == null) begin
+			$display("FATAL: uvmdev_mgr has not been instanced");
+		end
 		return m_inst;
 	endfunction
+	
+	task run_phase(uvm_phase phase);
+		m_run_phase = phase;
+	endtask
 	
 	static function uvmdev_if get(int unsigned id);
 		return inst().get_device(id);
